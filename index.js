2f7c63c06d64c86f82fcba103b132305d6c52b63
angelo suinan <angelo@mergecommit.com>
2019-04-06 15:22:10 +0800

    bifrost import

--- index.js
+++ index.js
@@ -2,8 +2,10 @@ const Stellar = require('./blockchain/Stellar')
 const Bounty = require('./blockchain/Bounty')
 const IOST = require('./blockchain/IOST')
 const StellarAsset = require('./blockchain/StellarAsset')
+const Bifrost = require('./blockchain/Bifrost')
 
 exports.Stellar = Stellar
 exports.Bounty = Bounty
 exports.IOST = IOST
 exports.StellarAsset = StellarAsset
+exports.Bifrost = Bifrost
